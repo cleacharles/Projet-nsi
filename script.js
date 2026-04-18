16985946e07243264ea3dab06c9acd4be28544b3
cleacharles <cleacharles7@gmail.com>
2026-04-19 00:23:20 +0200

    d

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
     const customSelectTrigger = document.querySelector("#custom-select")
     const customOptions = document.querySelector('#custom-options')
+    const customOptionsItems = document.querySelectorAll("#custom-options li")
+    const customDefault = document.querySelector("#custom-default")
     const hiddenInput = document.querySelector("#hiddenInput")
 
     customSelectTrigger.addEventListener('click', function(){
         customOptions.classList.toggle('open')
+    })
+
+    customOptionsItems.forEach(option => {
+        option.addEventListener("click", function(){
+
+        })
     })
 })
