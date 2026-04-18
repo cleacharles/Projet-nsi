639e0adb640338f23220e7c8b0cbc601938d433f
cleacharles <cleacharles7@gmail.com>
2026-04-19 00:06:56 +0200

    hhhhh

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-vgffbfbfbfbfb
+document.addEventListener("DOMContentLoaded", function(){
+    const customSelectTrigger = document.querySelector("#custom-select")
+    const customOptions = document.querySelector('#custom-options')
+
+    customSelectTrigger.addEventListener('click', function(){
+        customOptions.classList.toggle('open')
+    })
+})
