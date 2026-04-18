6e0fbebb19a00367aa2d454f03c21bb4039f368a
cleacharles <cleacharles7@gmail.com>
2026-04-19 00:16:22 +0200

    kk

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     const customSelectTrigger = document.querySelector("#custom-select")
     const customOptions = document.querySelector('#custom-options')
+    const hiddenInput = document.querySelector("#hiddenInput")
 
     customSelectTrigger.addEventListener('click', function(){
         customOptions.classList.toggle('open')
