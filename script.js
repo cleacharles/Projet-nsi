cf7abbf06b8d8a6600c0eac07e156ff8a7460377
Christophe Charles <christophe.charles@octo.com>
2026-04-19 19:43:56 +0200

    feat: Ajout click sur image

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+// Obtenir le panier
+function recupererPanier() {
+    const panier = localStorage.getItem('panier');
+    return panier
+        ? JSON.parse(panier)
+        : [];
+}
+
+// Enregistrer le panier
+function enregistrerPanier(panier) {
+    localStorage.setItem('panier', JSON.stringify(panier));
+    // mettre à jour le nombre de produits dans le panier
+}
+
+// Ajouter un produit au panier
+function ajouterAuPanier(produitId, quantite = 1) {
+    const panier = recupererPanier();
+    const produitExistant = panier.find(produit => produit.id === produitId);
+
+    if (produitExistant) {
+        produitExistant.quantite += quantite;
+    } else {
+        panier.push({ id: produitId, quantite: quantite });
+    }
+
+    enregistrerPanier(panier);
+}
+
+document.querySelectorAll(".liste-bougies img")
+    .forEach(function (image) {
+        image.addEventListener("click", function () {
+            ajouterAuPanier(image.id);
+        })
+    });
+
+
+
+
 document.addEventListener("DOMContentLoaded", function(){
     const customSelectTrigger = document.querySelector("#custom-select")
     const customOptions = document.querySelector('#custom-options')
@@ -20,9 +58,4 @@ document.addEventListener("DOMContentLoaded", function(){
     })
 })
 
-document.querySelectorAll(".liste-bougies img")
-    .forEach(function (image) {
-        image.addEventListener("click", function () {
-            alert("Bonjour ! Vous avez cliqué sur l'image.");
-        })
-    });
+
